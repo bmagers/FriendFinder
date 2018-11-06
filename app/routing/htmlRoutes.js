cdd4a7db70d20fc4850d946a186d4555e59c6a87
Bryan Magers <bmagers@users.noreply.github.com>
2018-11-06 11:04:21 -0800

    create JSON object from survey answers

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,5 @@
-module.exports = function(app, path) {
+var path = require("path");
+module.exports = function(app) {
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
