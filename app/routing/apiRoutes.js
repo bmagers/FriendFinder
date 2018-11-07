6459e6c4ff612b6e4d7b1bb0dec5ad5c939bb69e
Bryan Magers <bmagers@users.noreply.github.com>
2018-11-07 13:21:37 -0800

    save updated friends array to friends.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,31 @@
+var friendsData = require("../data/friends");
+var fs = require("fs");
+
 module.exports = function(app) {
+
   app.get("/api/friends", function(req, res) {
-    // display JSON of all possible friends
+    res.json(friendsData);
   });
+
   app.post("/api/friends", function(req, res) {
-    // handle incoming survey results
-    // handle compatibility logic
+    // compare req.body with each of friendsData
+    // var difference = 0;
+    // friendsData.forEach(function(item, index) {
+    //   difference += Math.abs(req.body.scores[index] - item.scores[index]);
+    //   // item.scores is array to compare with req.body.scores
+    // });
+
+
+    friendsData.push(req.body);
+    var updatedFriendsData = "var friendsArray = ";
+    updatedFriendsData += JSON.stringify(friendsData);
+    updatedFriendsData += ";\n\n";
+    updatedFriendsData += "module.exports = friendsArray;";
+    fs.writeFile(__dirname + "/../data/friends.js", updatedFriendsData, function(err) {
+      if (err) throw err;
+      res.json(req.body);
+    });
+    
   });
+
 }
