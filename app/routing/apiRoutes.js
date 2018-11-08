63bfceb942d64a6b3078f4c94fa9b9611f3b5fa5
Bryan Magers <bmagers@users.noreply.github.com>
2018-11-08 11:18:54 -0800

    clear form on modal close

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,27 +17,29 @@ module.exports = function(app) {
       var difference = 0;
 
       for (var i = 0; i < 10; i++) {
-        var yourScore = req.body.scores[i];
-        var theirScore = item.scores[1];
+        var yourScore = parseInt(req.body.scores[i]);
+        var theirScore = parseInt(item.scores[i]);
         if (yourScore !== 0 && theirScore !== 0) {
           difference += Math.abs(yourScore - theirScore);
+          console.log("you = " + yourScore + " them = " + theirScore + " and diff = " + Math.abs(yourScore - theirScore));
         }
       }
 
       var friend = {
         name: item.name,
-        photo: item.photoUrl,
+        photo: item.photo,
         difference: difference
       }
-
       friends.push(friend);
+
       message += item.name + " has a difference of " + difference + " from you.\n\n";
       smallestDifference = difference < smallestDifference ? difference : smallestDifference;
 
     });
 
     var bestFriend = friends.find(bestFriend => bestFriend.difference === smallestDifference);
-    message += "Your new best friend is " + bestFriend.name + " and here's a photo: " + bestFriend.photoUrl;
+    message += "Your new best friend is " + bestFriend.name + " and here's a photo: " + bestFriend.photo;
+    console.log(message);
 
     friendsData.push(req.body);
     var updatedFriendsData = "var friendsArray = " + JSON.stringify(friendsData) + ";\n\nmodule.exports = friendsArray;";
