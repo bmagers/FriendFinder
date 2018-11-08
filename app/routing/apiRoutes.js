5e3f364a237a668e8f174bf042128c5d96bb7829
Bryan Magers <bmagers@users.noreply.github.com>
2018-11-07 16:03:24 -0800

    friend matching algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,13 +8,29 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    // compare req.body with each of friendsData
-    // var difference = 0;
-    // friendsData.forEach(function(item, index) {
-    //   difference += Math.abs(req.body.scores[index] - item.scores[index]);
-    //   // item.scores is array to compare with req.body.scores
-    // });
-
+    var message = "";
+    var friends = [];
+    var smallestDifference = 50;
+    friendsData.forEach(function(item, index) {
+      var difference = 0;
+      for (var i = 0; i < 10; i++) {
+        var yourScore = req.body.scores[i];
+        var theirScore = item.scores[1];
+        if (yourScore !== 0 && theirScore !== 0) {
+          difference += Math.abs(req.body.scores[i] - item.scores[i]);
+        }
+      }
+      var friend = {
+        name: item.name,
+        photo: item.photoUrl,
+        difference: difference
+      }
+      friends.push(friend);
+      message += item.name + " has a difference of " + difference + " from you.\n\n";
+      smallestDifference = difference < smallestDifference ? difference : smallestDifference;
+    });
+    var bestFriend = friends.find(bestFriend => bestFriend.difference === smallestDifference);
+    message += "Your new best friend is " + bestFriend.name + " and here's a photo: " + bestFriend.photoUrl;
 
     friendsData.push(req.body);
     var updatedFriendsData = "var friendsArray = ";
@@ -23,7 +39,7 @@ module.exports = function(app) {
     updatedFriendsData += "module.exports = friendsArray;";
     fs.writeFile(__dirname + "/../data/friends.js", updatedFriendsData, function(err) {
       if (err) throw err;
-      res.json(req.body);
+      res.json(message);
     });
     
   });
